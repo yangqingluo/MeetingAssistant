663e9f344732f840ae9a54cdaf90c5cb2df38340
yangqingluo <84807330@qq.com>
2017-09-01 11:40:49 +0800

    udp广播数据实现

--- MeetingAssistant/MeetingAssistant/ViewController.m
+++ MeetingAssistant/MeetingAssistant/ViewController.m
@@ -7,8 +7,18 @@
 //
 
 #import "ViewController.h"
+#import "GCDAsyncSocket.h"
+#import "GCDAsyncUdpSocket.h"
+#import "UIViewController+HUD.h"
 
-@interface ViewController ()
+@interface ViewController ()<GCDAsyncSocketDelegate, GCDAsyncUdpSocketDelegate>
+
+@property (strong, nonatomic) IBOutlet UITextField *hostTextField;
+@property (strong, nonatomic) IBOutlet UITextField *portTextField;
+@property (strong, nonatomic) IBOutlet UITextField *messageTextField;
+@property (strong, nonatomic) IBOutlet UITextView *logTextView;
+
+@property (strong, nonatomic) GCDAsyncUdpSocket *udpSocket;
 
 @end
 
@@ -16,7 +26,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.title = @"会议小助手";
 }
 
 
@@ -25,5 +35,47 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)sendButtonAction:(UIButton *)sender {
+    if (!self.messageTextField.text.length) {
+        [self showHint:@"发送内容不能为空"];
+        return;
+    }
+    if (!self.portTextField.text.length) {
+        [self showHint:@"请输入端口号"];
+        return;
+    }
+    
+    int port = [self.portTextField.text intValue];
+    [self.udpSocket sendData:[self.messageTextField.text dataUsingEncoding:NSUTF8StringEncoding] toHost:self.hostTextField.text port:port withTimeout:-1 tag:0];
+}
+
+-(void)addTextToLog:(NSString *)str{
+    self.logTextView.text = [NSString stringWithFormat:@"%@\n%@", str, self.logTextView.text];
+    [self.logTextView scrollsToTop];
+}
+
+#pragma getter
+- (GCDAsyncUdpSocket *)udpSocket {
+    if (!_udpSocket) {
+        _udpSocket = [[GCDAsyncUdpSocket alloc] initWithDelegate:self delegateQueue:dispatch_get_main_queue()];
+        [_udpSocket enableBroadcast:YES error:nil];
+        [_udpSocket beginReceiving:nil];
+    }
+    
+    return _udpSocket;
+}
+
+#pragma GCDAsyncUdpSocketDelegate
+- (void)udpSocket:(GCDAsyncUdpSocket *)sock didReceiveData:(NSData *)data
+      fromAddress:(NSData *)address
+withFilterContext:(id)filterContext {
+    NSLog(@"receiveData：%@",data);
+    NSString *receive = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    [self addTextToLog:[NSString stringWithFormat:@"udpSocket didReceiveDataFrom: %@\n%@", [GCDAsyncUdpSocket hostFromAddress:address], receive]];
+}
+
+- (void)udpSocket:(GCDAsyncUdpSocket *)sock didSendDataWithTag:(long)tag {
+    [self addTextToLog:@"udpSocket didSendData."];
+}
 
 @end
