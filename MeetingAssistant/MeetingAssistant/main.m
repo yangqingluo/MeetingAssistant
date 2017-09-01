c3f72076ad57b4e5fb840ee0a6bca64ce71edb80
yangqingluo <84807330@qq.com>
2017-09-01 10:06:13 +0800

    first commit

---
+++ MeetingAssistant/MeetingAssistant/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MeetingAssistant
+//
+//  Created by 7kers on 2017/9/1.
+//  Copyright © 2017年 yangqingluo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
